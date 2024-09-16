0fc511a1cba740cfecfc7d522023d4954cd7a4f9
phaniteja25 <tejavaishu2001@gmail.com>
2024-09-15 22:23:03 -0500

    backend initialized

---
+++ backend/nutri-fit-app/src/main/java/com/ase/project/nutri_fit_app/NutriFitAppApplication.java
@@ -0,0 +1,15 @@
+package com.ase.project.nutri_fit_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriFitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriFitAppApplication.class, args);
+
+		System.out.println("Hello Spring Application");
+	}
+
+}
---
+++ backend/nutri-fit-app/src/test/java/com/ase/project/nutri_fit_app/NutriFitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ase.project.nutri_fit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriFitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
