196bb4cd9c75e5a39615acb25214f4a6195d56d5
NitishYamsani <nitishyamsani@gmail.com>
2024-10-27 14:48:57 -0500

    updated my user profile changes to main

---
+++ frontend/src/Components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const AboutUs = () => {
+    return (
+        <>
+        <Navbar />
+        </>
+    );
+};
+
+export default AboutUs;
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import profilepic from './user.png';
 
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="navbar-header">
-        <h1>NutriFit</h1>
+        <h1>NutriNova</h1>
       </div>
+      <div className = "navbar-contents">
+      <div className = "profile-logo"><Link to = "/userprofile"><img src ={profilepic} alt = "profile" className="logo"/></Link></div>
       <div className="navbar-links">
-        <a href="#">Home</a>
-        <a href="#">Add Meal</a>
-        <a href="#">Generate Plan</a>
-        <a href="#">About Us</a>
+        <a href="/dashboard">Home</a>
+        <a href="/mealloging">Add Meal</a>
+        <a href="/mealgenerator">Generate Plan</a>
+        <a href="/aboutus">About Us</a>
+      </div>
       </div>
     </div>
   );
