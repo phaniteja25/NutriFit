41563362e8e7b84e1112f167ac2c5c787065048a
phaniteja25 <tejavaishu2001@gmail.com>
2024-12-31 22:38:24 +0530

    reverted back the code

--- backend/nutri-fit-app/src/main/java/com/ase/project/nutri_fit_app/NutriFitAppApplication.java
+++ backend/nutri-fit-app/src/main/java/com/ase/project/nutri_fit_app/NutriFitAppApplication.java
@@ -21,18 +21,18 @@ public class NutriFitAppApplication {
 
 
 
-		Dotenv dotenv = Dotenv.load();
-
-		// Set environment variables for Spring Boot
-		System.setProperty("SPRING_APPLICATION_NAME", dotenv.get("SPRING_APPLICATION_NAME"));
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty(("SPRING_DATASOURCE_PASSWORD"),dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty(("SPRING_JPA_HIBERNATE_DDL_AUTO"),dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
-		System.setProperty(("SPRING_JPA_DATABASE_PLATFORM"),dotenv.get("SPRING_JPA_DATABASE_PLATFORM"));
-		System.setProperty(("GEMINI_API_URL"),dotenv.get("GEMINI_API_URL"));
-		System.setProperty(("GEMINI_API_KEY"),dotenv.get("GEMINI_API_KEY"));
-		System.setProperty(("API_KEY"),dotenv.get("API_KEY"));
+//		Dotenv dotenv = Dotenv.load();
+//
+//		// Set environment variables for Spring Boot
+//		System.setProperty("SPRING_APPLICATION_NAME", dotenv.get("SPRING_APPLICATION_NAME"));
+//		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+//		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+//		System.setProperty(("SPRING_DATASOURCE_PASSWORD"),dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+//		System.setProperty(("SPRING_JPA_HIBERNATE_DDL_AUTO"),dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
+//		System.setProperty(("SPRING_JPA_DATABASE_PLATFORM"),dotenv.get("SPRING_JPA_DATABASE_PLATFORM"));
+//		System.setProperty(("GEMINI_API_URL"),dotenv.get("GEMINI_API_URL"));
+//		System.setProperty(("GEMINI_API_KEY"),dotenv.get("GEMINI_API_KEY"));
+//		System.setProperty(("API_KEY"),dotenv.get("API_KEY"));
 
 
 		SpringApplication.run(NutriFitAppApplication.class, args);
